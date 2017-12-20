8731907c14cd62655c00178d846a0edd17b8666c
异阳 <228322991@qq.com>
2017-12-20 22:05:37 +0800

    up

--- dev/common/bulid.js
+++ dev/common/bulid.js
@@ -1,11 +1,20 @@
-function key(name){
+function file(name){
 	return `
 	 mkdir ${name} 
 	 cd ${name}
-	 touch ${name}.html
+	 touch index.html
 	 touch main.scss
 	 touch main.js 
 	 cd ../
 	 `;
 }
-key()
+file('')
+
+function common_file(name,type){
+	return `
+	 cd common
+	 touch ${name}.${type} 
+	 cd ../
+	 `;
+}
+common_file('','')
